264c9fe878535e84ad5f6cef3a586d2cf8d73cdc
snehaa666 <psneha.reddy0221@gmail.com>
2026-01-07 09:54:08 +0000

    test completed

--- tests/test_db_basic.py
+++ tests/test_db_basic.py
@@ -30,8 +30,8 @@ class TestDatabaseBasics(unittest.TestCase):
 
         # Check: does 'students.db' file exist?
         self.assertTrue(
-            os.path.exists("students.db"),
-            "students.db file is missing"
+            os.path.exists("master.db"),
+            "student.db file is missing"
         )
 
     def test_can_connect_and_list_tables(self):
@@ -42,7 +42,7 @@ class TestDatabaseBasics(unittest.TestCase):
         """
 
         # Open a connection to the SQLite database
-        conn = sqlite3.connect("students.db")
+        conn = sqlite3.connect("master.db")
 
         # Create a cursor to run SQL queries
         cur = conn.cursor()
